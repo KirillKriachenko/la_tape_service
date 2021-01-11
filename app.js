72f829a3f4fae399d2064df9788fd8afd4d367d7
KirillKriachenko <KirillKriachenko@gmail.com>
2021-01-11 18:21:30 -0500

    setting hbs templates

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 8089
 
-console.log(path.join(__dirname,'../public'))
+var xmlrpc = require('xmlrpc')
 
-app.use(express.static('public'))
+const publicDir = path.join(__dirname,'/public')
+const viewPath = path.join(__dirname,'/templates')
+app.set('view engine','hbs')
+app.set('views', viewPath)
 
-app.get('',(req, res) => {
-  res.send('<h1>Test</h1>')
+app.use(express.static(publicDir))
+console.log(publicDir)
+
+app.get('',(req,res) =>{
+    res.render('index',{title:'Main Menu'})
 })
+app.get('/tape/register',(req,res)=>{
+    res.render('registertape',{})
+})
+
 
 app.listen(port,() => console.log('Listening on port http://localhost:8089 ${port}'));
