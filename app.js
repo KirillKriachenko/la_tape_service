c274fc6bf8d2098877ad6c5cae462eedbd16b7d3
KirillKriachenko <KirillKriachenko@gmail.com>
2021-01-22 17:59:40 -0500

    update Tape data in Inventory module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 8089
@@ -9,86 +10,272 @@ var Odoo = require('odoo-xmlrpc')
 
 const publicDir = path.join(__dirname, '/public')
 const viewPath = path.join(__dirname, '/templates')
+
+
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
-
-var odoo = new Odoo({
-    url: 'https://odoo.livingart.ca',
-    port: '8443',
-    db: 'odoo.livingart.ca',
-    username: 'gregoryr@livingart.ca',
-    password: 'Gregory@123'
-})
 
 // var odoo = new Odoo({
 //     url: 'https://odoo.livingart.ca',
 //     port: '8443',
 //     db: 'odoo.livingart.ca',
-//     username: 'kirill@livingart.ca',
-//     password: 'a1b2Norm911me'
+//     username: 'gregoryr@livingart.ca',
+//     password: 'Gregory@123'
 // })
 
 
+var odoo = new Odoo({
+    url: 'https://odoo.livingart.ca',
+    port: '8443',
+    db: 'odoo.livingart.ca',
+    username: 'kirill@livingart.ca',
+    password: 'a1b2Norm911me'
+})
+
+
 app.use(express.static(publicDir))
-console.log(publicDir)
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('', (req, res) => {
     res.render('index', {title: 'Main Menu'})
 })
 app.get('/tape/template', (req, res) => {
-    const  TAPE_CATEGORY_ID = 448;
+    const TAPE_CATEGORY_ID = 448;
 
     odoo.connect(function (err) {
-        if (err) { return console.log(err); }
+        if (err) {
+            return console.log(err);
+        }
         console.log('Connected to Odoo server.');
         var inParams = [];
         inParams.push([['categ_id', '=', TAPE_CATEGORY_ID]]);
-        inParams.push(['name', 'qty_available','default_code','attribute_line_ids','product_variant_count', 'image_1920']); //fields
+        inParams.push(['name', 'qty_available', 'default_code', 'attribute_line_ids', 'product_variant_count', 'image_1920']); //fields
 
         var params = [];
         params.push(inParams);
         odoo.execute_kw('product.template', 'search_read', params, function (err, value) {
-            if (err) { return console.log(err); }
-            console.log('Result: ', value);
-            res.render('choosetemplate',{data:value})
+            if (err) {
+                return console.log(err);
+            }
+            // console.log('Result: ', value);
+            res.render('choosetemplate', {data: value})
         });
     });
 })
 
-app.get('/tape/products',(req,res) =>{
-    if(!req.query.template){
+app.post('/tape/template', (req, res, next) => {
+    console.log('POST METHOD')
+    console.log(req.body.list)
+    var list = JSON.parse(req.body.list)
+
+    var update_data = (callback) => {
+        for (var i = 0; i < list.length; i++) {
+            var id = list[i].id;
+            var counter = list[i].counter;
+            var quantity = list[i].qty;
+            var tmpl = list[i].tmpl;
+
+            console.log(quantity)
+
+
+            odoo.connect(function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log('Connected to Odoo server.');
+                var inParams = [];
+
+                inParams.push({'product_id': id, 'new_quantity': quantity, 'product_tmpl_id': tmpl})
+                var params = [];
+                params.push(inParams);
+                odoo.execute_kw('stock.change.product.qty', 'create', params, function (err, value) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log('Result: ', value);
+
+
+                    odoo.connect(function (err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        console.log('Connected to Odoo server.');
+                        var inParams = [];
+
+                        inParams.push(value)
+                        // inParams.push('read');
+                        // inParams.push(false); //raise_exception
+                        var params = [];
+                        params.push(inParams);
+                        odoo.execute_kw('stock.change.product.qty', 'change_product_qty', params, function (err, value) {
+                            if (err) {
+                                return console.log(err);
+                            }
+                            console.log('Result: ', value);
+
+
+                        });
+                    });
+
+                });
+            });
+
+            // var update_data = (callback) =>{
+            //     odoo.connect(function (err) {
+            //         if (err) {
+            //             return console.log(err);
+            //         }
+            //         console.log('Connected to Odoo server.');
+            //
+            //         var inParams = [];
+            //         inParams.push([['product_id', '=', parseInt(id)],['location_id','=',29]]);
+            //         inParams.push(['id']); //fields
+            //
+            //         var params = [];
+            //         params.push(inParams);
+            //         odoo.execute_kw('stock.quant', 'search_read', params, function (err, value) {
+            //             if (err) {
+            //                 return console.log(err);
+            //             }
+            //             console.log('Result: ', value[0].id);
+            //             callback(value[0].id)
+            //         });
+            //     });
+            // }
+            //
+            // update_data((dataID) =>{
+            //
+            //     console.log('DATA', dataID)
+            //     console.log('Quantity',quantity)
+            //     odoo.connect(function (err) {
+            //         if (err) { return console.log(err); }
+            //         console.log('Connected to Odoo server.');
+            //         console.log(dataID)
+            //         var inParams = [];
+            //         inParams.push([dataID]); //id to update
+            //         inParams.push({'inventory_quantity': quantity})
+            //         var params = [];
+            //         params.push(inParams);
+            //         odoo.execute_kw('stock.quant', 'write', params, function (err, value) {
+            //             if (err) { return console.log(err); }
+            //             console.log('Result: ', value);
+            //         });
+            //     });
+            //
+            // })
+        }
+        callback()
+    }
+    update_data((data) =>{
+        function redirect(){
+            res.redirect('/tape/template')
+        }
+        setTimeout(redirect,1500  )
+
+    })
+
+
+})
+
+app.get('/tape/products', (req, res) => {
+    if (!req.query.template) {
         return res.send({
-            error:'You must provice search term'
+            error: 'You must provice search term'
         })
     }
 
-    console.log(req.query.template)
-    odoo.connect(function (err) {
-        if (err) { return console.log(err); }
-        console.log('Connected to Odoo server.');
-        var inParams = [];
-        inParams.push([['product_tmpl_id', '=', parseInt(req.query.template)]]);
-        // inParams.push([['product_tmpl_id', '=', req.query.template]]);
-        // inParams.push(['name', 'qty_available','image_1920']); //fields
+    // console.log(req.query.template)
 
-        var params = [];
-        params.push(inParams);
-        odoo.execute_kw('product.product', 'search_read', params, function (err, value) {
+    var dataList = (callback) => {
+        odoo.connect(function (err) {
             if (err) {
-                console.log(err);
-                return res.send({
-                    error:err
-                })
+                return console.log(err);
             }
+            console.log('Connected to Odoo server.');
+            var inParams = [];
+            inParams.push([['product_tmpl_id', '=', parseInt(req.query.template)]]);
 
-            console.log('Result: ', value);
-            res.render('registertape',{data:value})
+            var params = [];
+            params.push(inParams);
+            odoo.execute_kw('product.product', 'search_read', params, function (err, value) {
+                if (err) {
+                    console.log(err);
+                    return res.send({
+                        error: err
+                    })
+                }
+
+                callback(value)
+            });
+
         });
-    });
+    }
+
+    dataList((data) => {
+        res.render('registertape', {data: data})
+    })
+
+
+    // odoo.connect(function (err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log('Connected to Odoo server.');
+    //     var inParams = [];
+    //     inParams.push([['product_tmpl_id', '=', parseInt(req.query.template)]]);
+    //
+    //     var params = [];
+    //     params.push(inParams);
+    //     odoo.execute_kw('product.product', 'search_read', params, function (err, value) {
+    //         if (err) {
+    //             console.log(err);
+    //             return res.send({
+    //                 error: err
+    //             })
+    //         }
+    //
+    //
+    //     });
+    //
+    // });
+
+    // res.render('registertape', {data: value})
 })
 
 
 app.listen(port, () => console.log('Listening on port http://localhost:8089 ${port}'));
+
+
+// odoo.connect(function (err) {
+//     if (err) { return console.log(err); }
+//     console.log('Connected to Odoo server.');
+//     var inParams = [];
+//     inParams.push([['product_tmpl_id', '=', parseInt(req.query.template)]]);
+//
+//     var params = [];
+//     params.push(inParams);
+//     var products = odoo.execute_kw('product.product', 'search_read', params, function (err, value) {
+//         if (err) {
+//             console.log(err);
+//             return res.send({
+//                 error:err
+//             })
+//         }
+//
+//         console.log(value)
+//
+//         // console.log('Result: ', value);
+//         // res.render('registertape',{data:value})
+//         return {
+//             value
+//         };
+//     });
+//     console.log(products)
+//     // res.render('registertape',{data:value})
+// });
 
 
 //     var server = xmlrpc.createServer({host:'localhost',port:9090})
--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,134 @@
 console.log('Client side javascript file is loaded!')
+var tapeList = []
+
+function plusTape(elementId) {
+
+    var counter = parseInt(document.getElementById("counter-" + elementId).innerText)
+    var quantity = parseInt(document.getElementById("quantity-" + elementId).innerText)
+    var tmplId = parseInt(document.getElementById("tmlp-id-"+elementId).innerText)
+
+    if (quantity > 0) {
+        quantity = quantity - 1
+        counter = counter + 1
+
+        if (quantity == 0) {
+            document.getElementById("plus-btn-" + elementId).style.visibility = "hidden";
+        }
+
+
+        document.getElementById("counter-" + elementId).innerHTML = counter
+        document.getElementById("quantity-" + elementId).innerHTML = quantity
+
+
+        var searchResult = searchInArray(elementId, tapeList)
+
+        if (searchResult == null) {
+            tapeList.push({
+                'id': elementId,
+                'counter': counter,
+                'qty':quantity,
+                'tmpl':tmplId
+            })
+        } else {
+            searchResult.counter = counter;
+            searchResult.qty = quantity;
+        }
+
+        console.log(tapeList)
+        if(tapeList.length > 0){
+            document.getElementById("register-btn").style.visibility = "visible";
+            document.getElementById("back-btn").style.visibility = "hidden";
+        }
+    }
+
+    updateInput('list');
+}
+
+function minusTape(elementId) {
+
+    var counter = parseInt(document.getElementById("counter-" + elementId).innerText)
+    var quantity = parseInt(document.getElementById("quantity-" + elementId).innerText)
+
+    if (counter > 0) {
+        quantity = quantity + 1
+        counter = counter - 1
+
+        if (quantity > 0) {
+            document.getElementById("plus-btn-"+elementId).style.visibility = "visible";
+
+        }
+
+        document.getElementById("counter-" + elementId).innerHTML = counter
+        document.getElementById("quantity-" + elementId).innerHTML = quantity
+
+        var searchResult = searchInArray(elementId, tapeList)
+
+        if (searchResult != null) {
+            searchResult.counter = counter;
+            searchResult.qty = quantity;
+
+            if (counter <= 0){
+                removeFromArray(elementId,tapeList)
+            }
+        }
+
+        var elements = countDataArray(tapeList)
+        if(elements <= 0){
+            document.getElementById("register-btn").style.visibility = "hidden";
+            document.getElementById("back-btn").style.visibility = "visible";
+        }
+
+        console.log(tapeList)
+    }
+
+    updateInput('list');
+}
+
+function searchInArray(nameKey, myArray) {
+
+    for (var i = 0; i < myArray.length; i++) {
+        if (myArray[i].id === nameKey) {
+            return myArray[i];
+        }
+    }
+
+    return null;
+}
+
+function removeFromArray(nameKey,myArray){
+    for (var i = 0; i < myArray.length; i++) {
+        if (myArray[i].id === nameKey) {
+            myArray.splice(i,1)
+        }
+    }
+}
+
+function countDataArray(myArray){
+    data_counter = 0
+    for (var i = 0; i < myArray.length; i++){
+        data_counter = data_counter + myArray[i].counter
+    }
+    return data_counter
+}
+
+
+function updateInput(inputID){
+    var input = document.getElementById(inputID);
+    input.value = ''
+    input.value = JSON.stringify(tapeList)
+}
+
+// fetch('/tape/template', {
+//
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         list:tapeList
+//         // user: {
+//         //     name: "John",
+//         //     email: "john@example.com"
+//         // }
+//     })
+// });
